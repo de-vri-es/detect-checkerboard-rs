7444996c4c4d13883634ba7039564a72f127d0b8
Maarten de Vries <maarten@de-vri.es>
2021-02-21 17:36:31 +0100

    Use finetuning in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use num::{Num, ToPrimitive};
 use std::path::PathBuf;
 use structopt::StructOpt;
 use structopt::clap::AppSettings;
+use float_ord::FloatOrd;
 
 mod corner_filter;
 mod peak_finder;
@@ -63,20 +64,36 @@ fn wait_space(events: &mut std::sync::mpsc::Receiver<show_image::event::WindowEv
 fn do_main(options: Options) -> Result<(), String> {
 	let image = image::open(&options.image)
 		.map_err(|e| format!("failed to open image: {}: {}", options.image.display(), e))?;
+	let image = image.resize(image.width() * 4, image.height() * 4, image::imageops::FilterType::Lanczos3);
+	let image = image.to_rgb8();
 
 	let min_area = image.width() as f32 * image.height() as f32 * options.min_area.powi(2);
 	let max_residual = image.width().min(image.height()) as f32 * options.max_residual;
 	println!("minimum tile area in pixels: {}", min_area);
 	println!("maximum average quadranglification residual in pixels: {}", max_residual);
 
-	let mut greyscale = image.to_luma8();
-	imageproc::contrast::equalize_histogram_mut(&mut greyscale);
+	let grayscale: image::GrayImage = image.convert();
+	let equalized = imageproc::contrast::equalize_histogram(&grayscale);
 
 	let window = show_image::create_window("visualization", Default::default()).unwrap();
 	let mut events = window.event_channel().unwrap();
 
+	let gradients = corner_filter::cornerness_map(&grayscale, 4);
+	let min = gradients.pixels().min_by_key(|p| FloatOrd(p[0])).unwrap()[0];
+	let max = gradients.pixels().max_by_key(|p| FloatOrd(p[0])).unwrap()[0];
+	let mut gradients_visual: image::GrayImage = image::ImageBuffer::new(gradients.width(), gradients.height());
+	eprintln!("min, max cornerness: {}, {}", min, max);
+	for (source, visual) in gradients.pixels().zip(gradients_visual.pixels_mut()) {
+		visual[0] = ((source[0] - min) / (max - min) * 255.0) as u8;
+	}
+
+	println!("showing gradients");
+	//let gradients: image::GrayImage = gradients.convert();
+	window.set_image("gradients", gradients_visual).unwrap();
+	wait_space(&mut events);
+
 	println!("showing binary image");
-	let binarized = imageproc::contrast::threshold(&greyscale, 40);
+	let binarized = imageproc::contrast::threshold(&equalized, 40);
 	window.set_image("binarized", binarized.clone()).unwrap();
 	wait_space(&mut events);
 
@@ -92,17 +109,14 @@ fn do_main(options: Options) -> Result<(), String> {
 	let mut contours = imageproc::contours::find_contours::<i32>(&dilated);
 	contours.retain(|c| c.points.len() >= 4 && c.parent == None);
 
-	let magenta = image::Rgb([255, 0, 255]);
+	let magenta = image::Rgb([200, 0, 200]);
 	let cyan = image::Rgb([0, 255, 255]);
+	let green = image::Rgb([0, 255, 0]);
 	let quads: Vec<_> = contours.iter().map(|c| fit_quadrangle(&c.points)).collect();
 
-	let mut visual: image::RgbImage = (&greyscale).convert();
+	let mut visual = image.clone();
 	for (contour, quad) in contours.iter().zip(quads.iter()) {
-		for (a, b) in contour.points.iter().zip(contour.points.iter().skip(1)) {
-			let a = (a.x, a.y);
-			let b = (b.x, b.y);
-			imageproc::drawing::draw_antialiased_line_segment_mut(&mut visual, a, b, magenta, imageproc::pixelops::interpolate);
-		}
+		//draw_polygon(&mut visual, &contour.points, magenta);
 
 		let bbox = match bounding_box(quad) {
 			Some(x) => x,
@@ -126,11 +140,18 @@ fn do_main(options: Options) -> Result<(), String> {
 			continue;
 		}
 
-		for (a, b) in quad.iter().zip(quad.iter().cycle().skip(1)) {
-			let a = (a.x, a.y);
-			let b = (b.x, b.y);
-			imageproc::drawing::draw_antialiased_line_segment_mut(&mut visual, a, b, cyan, imageproc::pixelops::interpolate);
-		}
+		//draw_polygon(&mut visual, quad, cyan);
+		let search_radius = area.sqrt() * 0.10;
+		println!("fine-tuning quads with search radius {}", search_radius);
+
+		let tweaked_quad = [
+			peak_finder::find_subpixel_peak(&gradients, quad[0].x, quad[0].y, search_radius.ceil() as i32, 4),
+			peak_finder::find_subpixel_peak(&gradients, quad[1].x, quad[1].y, search_radius.ceil() as i32, 4),
+			peak_finder::find_subpixel_peak(&gradients, quad[2].x, quad[2].y, search_radius.ceil() as i32, 4),
+			peak_finder::find_subpixel_peak(&gradients, quad[3].x, quad[3].y, search_radius.ceil() as i32, 4),
+		];
+
+		draw_subpixel_points(&mut visual, &tweaked_quad[..], magenta);
 	}
 
 	println!("showing {} contours", contours.len());
@@ -138,6 +159,41 @@ fn do_main(options: Options) -> Result<(), String> {
 	wait_space(&mut events);
 
 	Ok(())
+}
+
+fn draw_polygon<T, C>(image: &mut image::ImageBuffer<image::Rgb<u8>, C>, points: &[Point<i32>], color: image::Rgb<u8>)
+where
+	C: std::ops::Deref<Target = [u8]> + std::ops::DerefMut,
+	T: num::NumCast,
+{
+	for (a, b) in points.iter().zip(points.iter().cycle().skip(1)) {
+		let a = (a.x.to_i32().unwrap(), a.y.to_i32().unwrap());
+		let b = (b.x.to_i32().unwrap(), b.y.to_i32().unwrap());
+		imageproc::drawing::draw_antialiased_line_segment_mut(image, a, b, color, imageproc::pixelops::interpolate);
+	}
+}
+
+fn draw_points<C>(image: &mut image::ImageBuffer<image::Rgb<u8>, C>, points: &[Point<i32>], color: image::Rgb<u8>)
+where
+	C: std::ops::Deref<Target = [u8]> + std::ops::DerefMut,
+{
+	for point in points {
+		imageproc::drawing::draw_cross_mut(image, color, point.x.to_i32().unwrap(), point.y.to_i32().unwrap())
+	}
+}
+
+fn draw_subpixel_points<C>(image: &mut image::ImageBuffer<image::Rgb<u8>, C>, points: &[Point<f32>], color: image::Rgb<u8>)
+where
+	C: std::ops::Deref<Target = [u8]> + std::ops::DerefMut,
+{
+	for point in points {
+		imageproc::drawing::draw_line_segment_mut(image, (point.x - 8.0, point.y - 1.0), (point.x + 8.0, point.y - 1.0), color);
+		imageproc::drawing::draw_line_segment_mut(image, (point.x - 8.0, point.y + 0.0), (point.x + 8.0, point.y + 0.0), color);
+		imageproc::drawing::draw_line_segment_mut(image, (point.x - 8.0, point.y + 1.0), (point.x + 8.0, point.y + 1.0), color);
+		imageproc::drawing::draw_line_segment_mut(image, (point.x - 1.0, point.y - 8.0), (point.x - 1.0, point.y + 8.0), color);
+		imageproc::drawing::draw_line_segment_mut(image, (point.x + 0.0, point.y - 8.0), (point.x + 0.0, point.y + 8.0), color);
+		imageproc::drawing::draw_line_segment_mut(image, (point.x + 1.0, point.y - 8.0), (point.x + 1.0, point.y + 8.0), color);
+	}
 }
 
 fn bounding_box(points: &[Point<i32>]) -> Option<Rect> {
